d199a3303f22a8332120272c427c93154d01087b
tviskaron <tviskaron@gmail.com>
2025-07-07 21:22:49 +0300

    Add README with link to main POGEMA Toolbox repository

--- pogema_toolbox/generators/generator_utils.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-
-def maps_dict_to_yaml(filename, maps):
-    with open(filename, 'w') as file:
-        yaml.add_representer(str,
-                             lambda dumper, data: dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|'))
-        yaml.dump(maps, file)
